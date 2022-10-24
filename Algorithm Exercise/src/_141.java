2329f369de128e9e8225e41f4de290063fb54391
c99 <c99@gmail.com>
2022-10-24 11:10:04 +0700

    Bai tap Algorithm buoi 9

---
+++ Algorithm Exercise/src/_141.java
@@ -0,0 +1,23 @@
+public class _141 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
